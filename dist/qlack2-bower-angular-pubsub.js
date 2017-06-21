a928818889ec92a733be3827a35a5dfa2d07916c
Nassos A. Michas <nassosmichas@me.com>
2017-06-21 13:47:18 +0300

    Upgraded to qlack2-bower-pubsub 1.0.7

--- dist/qlack2-bower-angular-pubsub.js
+++ dist/qlack2-bower-angular-pubsub.js
@@ -5,29 +5,30 @@
 
 })(angular);
 
-(function(angular) {
-	'use strict';
+(function (angular) {
+  'use strict';
 
-	QNgPubSubService.$inject = ["$window", "$log"];
-	angular.module('QNgPubSub').factory('QNgPubSubService', QNgPubSubService);
+  QNgPubSubService.$inject = ["$window", "$log"];
+  angular.module('QNgPubSub').factory('QNgPubSubService', QNgPubSubService);
 
-	function QNgPubSubService($window, $log) {
-		// Keep a local reference to QPubSub service and delete it from window
-		// object, so that clients do not use it directly.
-		var QPubSub = $window.QPubSub;
+  function QNgPubSubService($window, $log) {
+    /** Keep a local reference to QPubSub service and delete it from window
+     *  object, so that clients do not use it directly.
+     */
+    var QPubSub = $window.QPubSub;
 
-		// Delete QPubSubService from window so it's not globally accessible.
-		if ($window.QPubSub) {
-			try {
-				delete $window.QPubSub;
-				$log.debug('QPubSub deleted from window object.');
-			} catch (e) {
-				$window.QPubSub = undefined;
-				$log.debug('QPubSub undefined from window object.');
-			}
-		}
+    /** Delete QPubSubService from window so it's not globally accessible. */
+    if ($window.QPubSub) {
+      try {
+        delete $window.QPubSub;
+        $log.debug('QPubSub deleted from window object.');
+      } catch (e) {
+        $window.QPubSub = undefined;
+        $log.debug('QPubSub undefined from window object.');
+      }
+    }
 
-		return QPubSub;
-	}
+    return QPubSub;
+  }
 
 })(angular);
